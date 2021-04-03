d21cef757201d98c3a21ddbf826b11de9e0e93db
nataliaSzczecinska <nataliaszczecinska@o2.pl>
2021-04-03 19:11:25 +0200

    Task JDP210302-1 + project structure

---
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.kodilla.ecommercee.controller;
+
+import com.kodilla.ecommercee.domain.ProductDto;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.math.BigDecimal;
+import java.util.*;
+
+@RestController
+@RequestMapping("/v1/product")
+public class ProductController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+
+    @GetMapping(value = "getProducts")
+    public List<ProductDto> getProducts() {
+        LOGGER.info("Product list has been displayed");
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "getProduct")
+    public ProductDto getProduct(Long productId) {
+        LOGGER.info("Product has been displayed");
+        return new ProductDto(1L,
+                "Test product",
+                "Test product description",
+                new BigDecimal(100.0));
+    }
+
+    @DeleteMapping(value = "deleteProduct")
+    public void deleteProduct(Long productId) {
+        LOGGER.info("Product has been deleted");
+    }
+
+    @PutMapping(value = "updateProduct")
+    public ProductDto updateProduct(ProductDto productDto) {
+        LOGGER.info("Product has been updated");
+        return new ProductDto(1L,
+                "Test update product",
+                "Test update description",
+                new BigDecimal(150.0));
+    }
+
+    @PostMapping(value = "createProduct")
+    public void createProduct(ProductDto productDto) {
+        LOGGER.info("Product has been created");
+    }
+}
---
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -0,0 +1,54 @@
+package com.kodilla.ecommercee.domain;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private Long productId;
+    private String productName;
+    private String productDescription;
+    private BigDecimal price;
+    //private ProductGroup productGroup;
+
+
+    public Product(Long productId, String productName, String productDescription, BigDecimal price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.price = price;
+    }
+
+    public Product() {
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
